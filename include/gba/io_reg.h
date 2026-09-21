54bfb79ea1fdff23264383e13eca1056359642c0
ThaBombs <68473344+ThaBombs@users.noreply.github.com>
2026-09-22 00:11:58 +0200

    Native platform: make IO registers host-backed

--- include/gba/io_reg.h
+++ include/gba/io_reg.h
@@ -1,7 +1,11 @@
 #ifndef GUARD_GBA_IO_REG_H
 #define GUARD_GBA_IO_REG_H
 
+#ifndef PORTABLE
 #define REG_BASE 0x4000000 // I/O register base address
+#else
+extern unsigned char REG_BASE[];
+#endif
 
 // I/O register offsets
 
