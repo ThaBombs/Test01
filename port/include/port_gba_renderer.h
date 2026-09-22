d1ccf331e86e1d7363efcfca99c1d98612f1ee53
ThaBombs <68473344+ThaBombs@users.noreply.github.com>
2026-09-22 10:14:24 +0200

    Expose compatibility surface renderer

--- port/include/port_gba_renderer.h
+++ port/include/port_gba_renderer.h
@@ -8,6 +8,7 @@
 #define PORT_GBA_FRAME_HEIGHT 160
 
 void PortGbaRenderer_RenderCompat(uint32_t *pixels);
+bool PortGbaRenderer_RenderSurface(uint32_t *pixels, int width, int height, int stridePixels);
 bool PortGbaRenderer_SelfTest(void);
 
 #endif // POKEEMERALD_PORT_GBA_RENDERER_H
