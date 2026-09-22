1f5f84f3ee2d9a4e30536f4328d440de4fcfef00
ThaBombs <68473344+ThaBombs@users.noreply.github.com>
2026-09-22 08:54:35 +0200

    Native platform: restore DMA mask constants

--- include/gba/io_reg.h
+++ include/gba/io_reg.h
@@ -651,9 +651,11 @@ extern unsigned char REG_BASE[];
 #define DMA_DEST_DEC      0x0020
 #define DMA_DEST_FIXED    0x0040
 #define DMA_DEST_RELOAD   0x0060
+#define DMA_DEST_MASK     0x0060
 #define DMA_SRC_INC       0x0000
 #define DMA_SRC_DEC       0x0080
 #define DMA_SRC_FIXED     0x0100
+#define DMA_SRC_MASK      0x0180
 #define DMA_REPEAT        0x0200
 #define DMA_16BIT         0x0000
 #define DMA_32BIT         0x0400
