daf6a004435f5a08f356e81ada4daa54a8a8ce11
ThaBombs <68473344+ThaBombs@users.noreply.github.com>
2026-09-23 12:23:08 +0200

    Declare Android overworld port types

--- port/include/port_test_overworld.h
+++ port/include/port_test_overworld.h
@@ -1,6 +1,8 @@
 #ifndef GUARD_PORT_TEST_OVERWORLD_H
 #define GUARD_PORT_TEST_OVERWORLD_H
 
+#include "gba/types.h"
+
 void PortGame_StartTestOverworld(void);
 void PortGame_LoadTestMap(u16 mapGroup, u16 mapNum, s16 focusX, s16 focusY);
 bool32 PortGame_TryTestWarpAt(s16 x, s16 y);
