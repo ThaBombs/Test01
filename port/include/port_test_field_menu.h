17183d9c93beb8daf43a43e6ef96b2aed75b1292
ThaBombs <68473344+ThaBombs@users.noreply.github.com>
2026-09-23 12:23:15 +0200

    Declare Android field menu port types

--- port/include/port_test_field_menu.h
+++ port/include/port_test_field_menu.h
@@ -1,6 +1,8 @@
 #ifndef GUARD_PORT_TEST_FIELD_MENU_H
 #define GUARD_PORT_TEST_FIELD_MENU_H
 
+#include "gba/types.h"
+
 void PortTestFieldMenu_Init(void);
 bool32 PortTestFieldMenu_Update(void);
 void PortTestFieldMenu_Close(void);
