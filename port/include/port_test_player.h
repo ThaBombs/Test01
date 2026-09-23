bc4cb8d4c9215534b0063688b0ed0622c2461277
ThaBombs <68473344+ThaBombs@users.noreply.github.com>
2026-09-23 22:23:12 +0200

    Type player facing helper for Android

--- port/include/port_test_player.h
+++ port/include/port_test_player.h
@@ -1,6 +1,8 @@
 #ifndef GUARD_PORT_TEST_PLAYER_H
 #define GUARD_PORT_TEST_PLAYER_H
 
+#include "gba/types.h"
+
 void PortTestPlayer_Init(void);
 void PortTestPlayer_Update(void);
 void PortTestPlayer_SetFacingDirection(u8 direction);
