0e5fef0cab9b9c9815defce55521d3de8a14cb3f
ThaBombs <68473344+ThaBombs@users.noreply.github.com>
2026-09-23 22:05:43 +0200

    Expose Android map connection loader

--- port/include/port_test_overworld.h
+++ port/include/port_test_overworld.h
@@ -10,6 +10,7 @@
 
 void PortGame_StartTestOverworld(void);
 void PortGame_LoadTestMap(u16 mapGroup, u16 mapNum, s16 focusX, s16 focusY);
+void PortGame_LoadTestConnectionMap(u16 mapGroup, u16 mapNum);
 bool32 PortGame_TryTestWarpAt(s16 x, s16 y);
 bool32 PortGame_TryTestCoordEventAt(s16 x, s16 y);
 void PortGame_OpenTestOptions(void);
